f7d9e57cdcab6b7db02d19c71bdf5eb304fe0703
Lama Alsaif <lama.alsaif99@gmail.com>
2021-09-11 21:20:05 +0100

    Update Runner.py

--- Runner.py
+++ Runner.py
@@ -2,13 +2,24 @@ import pygame
 from sys import exit
 
 from pygame.time import Clock
+
+def display_score():
+    current_time=int(pygame.time.get_ticks()/1000) - satrt_time 
+    score_surf = test_font.render(f'Score {current_time}', False, ((64),(64),(64)))
+    score_rect = score_surf.get_rect(center = (400,50))
+    Screen.blit(score_surf, score_rect)
+
 pygame.init()
+
+
+
 
 Screen = pygame.display.set_mode((800, 400))#width , height
 pygame.display.set_caption("first game")
 clock = pygame.time.Clock()
 test_font  = pygame.font.Font('font/Pixeltype.ttf' , 50)#(font type , font size)
 game_active = True
+satrt_time = 0
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert_alpha()
 ground_surface=pygame.image.load('graphics/ground.png').convert_alpha()
@@ -18,8 +29,9 @@ player_surf = pygame.image.load('graphics\Player\player_walk_1.png').convert_alp
 player_rect = player_surf.get_rect(midbottom = (80 , 300))
 player_gravity = 0
 
-score_surf= test_font.render('My game', False , (64 , 64 , 64))#text , AA ,color
-score_rect = score_surf.get_rect( center = (400 , 50))
+# score_surf= test_font.render('My game', False , (64 , 64 , 64))#text , AA ,color
+# score_rect = score_surf.get_rect( center = (400 , 50))
+
 #surface= pygame.Surface((100,200)) #W,H (display surface and regular surface have alot of things in commen)
 #surface.fill('Red')
 
@@ -37,6 +49,7 @@ while True :
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active =True
                 snail_rect.left = 600
+                satrt_time = int(pygame.time.get_ticks()/1000)
 
         
 
@@ -46,9 +59,11 @@ while True :
     if game_active:
         Screen.blit(sky_surface,(0,0))
         Screen.blit(ground_surface,(0,300))
-        pygame.draw.rect(Screen , '#c0e8ec' , score_rect )
-        pygame.draw.rect(Screen , '#c0e8ec' , score_rect, 10 )
-        Screen.blit(score_surf , score_rect)
+        # pygame.draw.rect(Screen , '#c0e8ec' , score_rect )
+        # pygame.draw.rect(Screen , '#c0e8ec' , score_rect, 10 )
+        # Screen.blit(score_surf , score_rect)
+
+        display_score()
 
         snail_rect.left -= 4
         if snail_rect.right <= 0 : snail_rect.left = 800
